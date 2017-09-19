f61a4484c4fdfde84a86c53e02ed3e8b5577e1d5
Jeannie <jflaherty@utexas.edu>
2017-09-19 17:43:26 -0500

    clean seeds and schema files

--- db/schema.sql
+++ db/schema.sql
@@ -1,23 +1,12 @@
-CREATE DATABASE burgers_db;
+DROP DATABASE IF EXISTS burgers_db;
 
-USE burgers_db;
+CREATE DATABASE burgers_db;
 
 CREATE TABLE burgers
 (
  id int NOT NULL AUTO_INCREMENT,
  burger_name varchar(255) NOT NULL,
- devoured BOOLEAN,
+ devoured BOOLEAN default false,
  date TIMESTAMP,
  PRIMARY KEY (id)
 );
-
--- CREATE TABLE parties
--- (
---  id int NOT NULL AUTO_INCREMENT,
---  party_name varchar(255) NOT NULL,
---  party_type varchar(255) NOT NULL,
---  party_cost int NOT NULL,
---  client_id int NOT NULL,
---  PRIMARY KEY (id),
---  FOREIGN KEY (client_id) REFERENCES clients(id)
--- );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,19 +1,3 @@
-DROP DATABASE IF EXISTS burgers_db;
-
-CREATE DATABASE burgers_db;
-
-CREATE TABLE burgers
-(
- id int NOT NULL AUTO_INCREMENT,
- burger_name varchar(255) NOT NULL,
- devoured BOOLEAN default false,
- date TIMESTAMP,
- PRIMARY KEY (id)
-);
-
--- INSERT INTO clients (client_name) VALUES ('Bilal');
--- INSERT INTO clients (client_name) VALUES ('Brianne');
--- INSERT INTO clients (client_name) VALUES ('Vincent');
 
 INSERT INTO burgers (burger_name) VALUES ('Cheese');
 INSERT INTO burgers (burger_name) VALUES ('Bacon');
