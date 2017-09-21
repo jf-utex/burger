095efcf3cfdfce665ccac966d24f0172bb21fc99
Jeannie <jflaherty@utexas.edu>
2017-09-20 19:25:59 -0500

    code not rendering to page - connection is established

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,8 @@ DROP DATABASE IF EXISTS burgers_db;
 
 CREATE DATABASE burgers_db;
 
+USE burgers_db;
+
 CREATE TABLE burgers
 (
  id int NOT NULL AUTO_INCREMENT,
@@ -10,3 +12,15 @@ CREATE TABLE burgers
  date TIMESTAMP,
  PRIMARY KEY (id)
 );
+
+-- // To run form BASH
+-- * Make sure you're in the `db` folder of your app.
+--
+--    * Start MySQL command line tool and login: `mysql -u root -p`.
+--
+--    * With the `mysql>` command line tool running, enter the command `source schema.sql`. This will run your schema file and all of the queries in it -- in other words, you'll be creating your database.
+--
+--    * Now insert the entries you defined in `seeds.sql` by running the file: `source seeds.sql`.
+--
+--    * Close out of the MySQL command line tool: `exit`.
+--
